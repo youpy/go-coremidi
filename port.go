d92e285d78c87896679cec9b7f856152cf53d8ac
youpy <youpy@buycheapviagraonlinenow.com>
2013-02-09 00:10:55 +0900

    add InputPort

--- port.go
+++ port.go
@@ -1,19 +1,80 @@
 package coremidi
 
-/*
-#cgo LDFLAGS: -framework CoreMIDI -framework CoreFoundation
-#include <CoreMIDI/CoreMIDI.h>
-*/
-import "C"
 import "unsafe"
 import "errors"
 import "fmt"
 
-type Port struct {
+/*
+#cgo LDFLAGS: -framework CoreMIDI -framework CoreFoundation
+#include <CoreMIDI/CoreMIDI.h>
+#include <stdio.h>
+#include <unistd.h>
+
+extern void goCallback(void *proc, char *value);
+
+static int getIntValue(int *array, int index)
+{
+  return array[index];
+}
+
+static void readFromPipeAndCallback(int fd, void *proc)
+{
+  int n;
+  char readbuffer[1024];
+
+  while((n = read(fd, readbuffer, sizeof(readbuffer) - 1)) > 0) {
+    readbuffer[n] = 0x00;
+
+    goCallback(proc, readbuffer);
+  }
+}
+
+static int *make_pipe()
+{
+  int *fd = calloc(sizeof(int), 2);
+
+  pipe(fd);
+
+  return fd;
+}
+
+static void MIDIInputProc(const MIDIPacketList *pktlist, void *readProcRefCon,  void *srcConnRefCon)
+{
+  MIDIPacket *packet = (MIDIPacket *)&(pktlist->packet[0]);
+  UInt32 packetCount = pktlist->numPackets;
+  int i, n;
+
+  for (i = 0; i < packetCount; i++) {
+    n = write(*(int *)srcConnRefCon, &packet->data, packet->length);
+    packet = MIDIPacketNext(packet);
+  }
+}
+
+typedef void (*midi_input_proc)(const MIDIPacketList *pktlist, void *readProcRefCon, void *srcConnRefCon);
+
+static midi_input_proc getProc()
+{
+  return *MIDIInputProc;
+}
+
+*/
+import "C"
+
+//export goCallback
+func goCallback(proc unsafe.Pointer, p1 *C.char) {
+	foo := *(*func([]byte))(proc)
+
+	foo(([]byte)(C.GoString(p1)))
+}
+
+type OutputPort struct {
 	port C.MIDIPortRef
 }
 
-type OutputPort Port
+type InputPort struct {
+	port     C.MIDIPortRef
+	readProc func([]byte)
+}
 
 func NewOutputPort(client Client, name string) (outputPort OutputPort, err error) {
 	var port C.MIDIPortRef
@@ -30,4 +91,41 @@ func NewOutputPort(client Client, name string) (outputPort OutputPort, err error
 	}
 
 	return
+}
+
+func NewInputPort(client Client, name string, readProc func([]byte)) (inputPort InputPort, err error) {
+	var port C.MIDIPortRef
+
+	cName := C.CString(name)
+	defer C.free(unsafe.Pointer(cName))
+
+	osStatus := C.MIDIInputPortCreate(client.client,
+		C.CFStringCreateWithCString(nil, cName, C.kCFStringEncodingMacRoman),
+		(C.MIDIReadProc)(unsafe.Pointer(C.getProc())),
+		unsafe.Pointer(uintptr(0)),
+		&port)
+
+	if osStatus != C.noErr {
+		err = errors.New(fmt.Sprintf("%d: failed to create a port", int(osStatus)))
+	} else {
+		inputPort = InputPort{port, readProc}
+	}
+
+	return
+}
+
+func (port InputPort) Connect(source Source) {
+	fd := pipe()
+	writeFd := C.getIntValue(fd, 1)
+
+	C.MIDIPortConnectSource(port.port, source.endpoint, unsafe.Pointer(&writeFd))
+
+	go func() {
+		// TODO: should terminate when MIDIPortDisconnectSource is called
+		C.readFromPipeAndCallback(C.getIntValue(fd, 0), unsafe.Pointer(&port.readProc))
+	}()
+}
+
+func pipe() *C.int {
+	return C.make_pipe()
 }
--- port_test.go
+++ port_test.go
@@ -1,6 +1,7 @@
 package coremidi
 
 import "testing"
+import "fmt"
 
 func TestNewOutputPort(t *testing.T) {
 	client, err := NewClient("test")
@@ -14,4 +15,23 @@ func TestNewOutputPort(t *testing.T) {
 	if err != nil {
 		t.Fatalf("failed to create a port")
 	}
+}
+
+func TestNewInputPort(t *testing.T) {
+	client, _ := NewClient("test")
+
+	port, err := NewInputPort(client, "test", func(value []byte) {
+		fmt.Printf("value: %v\n", value)
+	})
+
+	if err != nil {
+		t.Fatalf("failed to create a port")
+	}
+
+	sources, _ := AllSources()
+
+	port.Connect(sources[0])
+
+	packet := NewPacket([]byte{0x90, 0x30, 100})
+	packet.Received(&sources[0])
 }
